96a577e9ef8406a62cc570644b4b178944282137
nasser <hekmati.nasser@gmail.com>
2023-05-21 11:18:27 +0330

    fix: request facades

--- src/Facades/RequestBuilder.php
+++ src/Facades/RequestBuilder.php
@@ -2,13 +2,14 @@
 namespace Celysium\Request\Facades;
 
 use Closure;
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Pool;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static mixed request()
- * @method static mixed batch(Closure $callback)
- * @method static mixed async(Pool $pool, string $key)
+ * @method static PendingRequest request($to = null)
+ * @method static array batch(Closure $callback)
+ * @method static PendingRequest async(Pool $pool, string $key)
  */
 class RequestBuilder extends Facade
 {
